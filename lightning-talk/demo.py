302d2cc186e6f677dcf29c84d70ccdc61d4cbedb
Elliott Turner <ertnxt@gmail.com>
2022-04-06 01:43:14 -0500

    add (slow) pitch data graphing

--- lightning-talk/demo.py
+++ lightning-talk/demo.py
@@ -1,6 +1,9 @@
 import pybullet as p
 import time
 import pybullet_data
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
 physics_client = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
@@ -13,11 +16,46 @@ bot_id = p.loadURDF("lightning-talk/bot.urdf",start_pos, start_orientation)
 left_wheel_input = p.addUserDebugParameter('left wheel', -25, 25, 0)
 right_wheel_input = p.addUserDebugParameter('right wheel', -25, 25, 0)
 
+plt.ion()
+fig = plt.figure()
+fig.set_tight_layout(True)
+ax = fig.add_subplot(1, 1, 1)
+ax.yaxis.set_minor_locator(MultipleLocator(10))
+ax.xaxis.set_minor_locator(MultipleLocator(5))
+plt.title('Pitch vs. Simulation Steps')
+plt.xlabel('Steps Since Simulation Start')
+plt.ylabel('Pitch')
+plt.grid()
+
+buffer_size = 25
+x = [-2, -1]
+y = [0, 0]
+
+line, = ax.plot(x, y)
+ax.set_ylim(-180, 180)
+
+fig.canvas.draw()
+
 for i in range (10000):
     p.setJointMotorControl2(bot_id, 0, p.VELOCITY_CONTROL,
         targetVelocity=p.readUserDebugParameter(left_wheel_input))
     p.setJointMotorControl2(bot_id, 1, p.VELOCITY_CONTROL,
         targetVelocity=p.readUserDebugParameter(right_wheel_input))
+
+    pitch = p.getEulerFromQuaternion(p.getBasePositionAndOrientation(bot_id)[1])[1]*180/3.1415
+
+    if i%5 == 0:
+        x.append(i)
+        y.append(pitch)
+        if len(x) > buffer_size:
+            x.pop(0)
+            y.pop(0)
+
+        line.set_data(x, y)
+        ax.set_xlim(x[0], x[-1])
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
     p.stepSimulation()
     time.sleep(1./500.)
 p.disconnect()
