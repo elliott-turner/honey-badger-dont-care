5d87a1afa87ba2e4bfd66c52ece70bfbbee68d98
Elliott Turner <ertnxt@gmail.com>
2022-04-07 01:15:50 -0500

    very basic PID implementation

--- lightning-talk/demo.py
+++ lightning-talk/demo.py
@@ -13,8 +13,12 @@ start_orientation = p.getQuaternionFromEuler([0,0,0])
 bot_id = p.loadURDF("lightning-talk/bot.urdf",start_pos, start_orientation)
 #set the center of mass frame (loadURDF sets base link frame) start_pos/Ornp.resetBasePositionAndOrientation(bot_id, start_pos, start_orientation)
 
-left_wheel_input = p.addUserDebugParameter('left wheel', -25, 25, 0)
-right_wheel_input = p.addUserDebugParameter('right wheel', -25, 25, 0)
+turn_input = p.addUserDebugParameter('turn', -25, 25, 0)
+traverse_input = p.addUserDebugParameter('traverse', -25, 25, 0)
+
+p_input = p.addUserDebugParameter('P', 0, 50, 10)
+i_input = p.addUserDebugParameter('I', 0, 50, 10)
+d_input = p.addUserDebugParameter('D', 0 , 50, 10)
 
 plt.ion()
 fig = plt.figure()
@@ -33,23 +37,37 @@ y = [0 for _ in range(buffer_size)]
 
 line, = ax.plot(x, y)
 ax.set_ylim(-180, 180)
-
 fig.canvas.draw()
+
+Kp = 0.0
+Ki = 0.0
+Kd = 0.0
+def pid():
+    P = y[-1]/180.0
+    I = sum([yi/180.0 for yi in y])
+    D = y[-1]/180.0 - y[-2]/180.0
+    return -1*(Kp*P + Ki*I + Kd*D)
 
 i = 1
 while True:
-    p.setJointMotorControl2(bot_id, 0, p.VELOCITY_CONTROL,
-        targetVelocity=p.readUserDebugParameter(left_wheel_input))
-    p.setJointMotorControl2(bot_id, 1, p.VELOCITY_CONTROL,
-        targetVelocity=p.readUserDebugParameter(right_wheel_input))
-
     pitch = p.getEulerFromQuaternion(p.getBasePositionAndOrientation(bot_id)[1])[1]*180/3.1415
+
+    y.append(pitch)
+    y.pop(0)
+
+    Kp = p.readUserDebugParameter(p_input)
+    Ki = p.readUserDebugParameter(i_input)
+    Kd = p.readUserDebugParameter(d_input)
+    ctrl = pid()
+
+    turn = p.readUserDebugParameter(turn_input)
+    traverse = p.readUserDebugParameter(traverse_input)
+
+    p.setJointMotorControl2(bot_id, 0, p.VELOCITY_CONTROL, targetVelocity=ctrl+turn+traverse)
+    p.setJointMotorControl2(bot_id, 1, p.VELOCITY_CONTROL, targetVelocity=ctrl-turn+traverse)
 
     if i >= 5:
         i = 1
-        y.append(pitch)
-        y.pop(0)
-
         line.set_data(x, y)
         ax.set_xlim(x[0], x[-1])
         fig.canvas.draw()
