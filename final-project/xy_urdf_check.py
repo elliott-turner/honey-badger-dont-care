cfc5095535fe1dc0bb584e3c366e5b9ba870ed28
Elliott Turner <ertnxt@gmail.com>
2022-04-27 22:48:20 -0500

    make both axes move freely

--- final-project/xy_urdf_check.py
+++ final-project/xy_urdf_check.py
@@ -16,8 +16,14 @@ planeId = p.loadURDF("plane.urdf")
 start_pos = [0,0,1] 
 start_orientation = p.getQuaternionFromEuler([0,0,0]) 
 bot_id = p.loadURDF("xycartpole.urdf",start_pos, start_orientation) 
-p.getJointInfo(bot_id, 0)
-p.setJointMotorControl2(bot_id, 1, p.POSITION_CONTROL, force=0)
+
+y_joint_id = 0
+x_joint_id = 1
+stick_x_joint_id = 2
+stick_y_joint_id = 3
+
+p.setJointMotorControl2(bot_id, stick_x_joint_id, p.POSITION_CONTROL, force=0)
+p.setJointMotorControl2(bot_id, stick_y_joint_id, p.POSITION_CONTROL, force=0)
 
 for i in range (10000):
     p.stepSimulation()
