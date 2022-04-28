05a976e759763d5167336a9b3e58c2e6f1a1fa1b
Minsoo Kang <minsoo_kang@utexas.edu>
2022-04-28 02:32:23 -0500

    Completed PID control in 1D.

--- final-project/PIDTest.py
+++ final-project/PIDTest.py
@@ -1,7 +1,6 @@
 import pybullet as p
 import time
 import pybullet_data
-import random
 
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
@@ -24,25 +23,37 @@ last_error = 0
 
 # setup user inputs
 velocity_input = p.addUserDebugParameter('velocity', -5, 5, 0)
-p_input = p.addUserDebugParameter('P', 0, 10, 8)  # changed - these values were high
-i_input = p.addUserDebugParameter('I', 0, 10, 5)  # changed - these values were high
+p_input = p.addUserDebugParameter('P', 0, 100, 100)  # changed - these values were high
+i_input = p.addUserDebugParameter('I', 0, 100, 100)  # changed - these values were high
 d_input = p.addUserDebugParameter('D', 0, 1, .05)  # changed - these values were high
-# for i in range (10000):
-#     p.stepSimulation()
-#     time.sleep(.005)
 
 tilt_data = [0]
 error_data = [0]
-link_state = []
 step = 0
+
+g = -10
+baseV = [0]
+baseA = [0]
+
+# calculate and store PID values
+def PID(error, error_data, tilt_data, P, I, D):
+
+    P = error
+
+    I += (error_data[step] - error_data[step - 1]) / 2
+
+    D = -1 * (tilt_data[step] - tilt_data[step - 1])
+
+    ctrl = (Kp * P + Ki * I + Kd * D)
+
+    return ctrl
+
+def velocityControl():
+
 
 while True:  # main loop
     # get and store tilt data from robot
     step += 1
-
-    wind = random.uniform(-2, 2)
-
-    p.applyExternalForce(bot_id, 1, (wind, 0, 0), (0, 0, 0), p.LINK_FRAME)
 
     # 0 to pi/2 to 0 to -pi/2
     tilt = p.getEulerFromQuaternion(p.getLinkState(bot_id, 1)[1])[1]
@@ -58,38 +69,12 @@ while True:  # main loop
     Ki = p.readUserDebugParameter(i_input)
     Kd = p.readUserDebugParameter(d_input)
 
-    # calculate and store PID values
-    P = error
+    baseV.append(p.getBaseVelocity(bot_id)[0][0])
+    baseA.append(baseV[step] - baseV[step])
 
-    I += (error_data[step] - error_data[step - 1])/2
+    cart_vel = PID(error, error_data, tilt_data, P, I, D) + velocity
 
-    D = -1 * (tilt_data[step] - tilt_data[step - 1])
-
-    # for v,j in zip([P,I,D], range(1,4)): # save PID data for plotting
-    #     buffers[j][0].append(v)
-    #     buffers[j][0].pop(0)
-
-    # calculate and store control output based on PID values and coefficients
-    ctrl = (Kp * P + Ki * I + Kd * D)
-
-    cart_vel = ctrl + velocity
-    print(cart_vel, tilt, step)
-
-    # for v,j in zip([ctrl, ml, mr], range(3)): # save motor control data for plotting
-    #     buffers[4][j].append(v)
-    #     buffers[4][j].pop(0)
-
-    # # set robot motor target speeds
-    # p.setJointMotorControl2(bot_id, 0, p.VELOCITY_CONTROL, targetVelocity=ml)
     p.setJointMotorControl2(bot_id, 0, p.VELOCITY_CONTROL, targetVelocity=-cart_vel)
-
-    # # update graphs
-    # for ax, line, buff, background in zip(axs, lines, buffers, backgrounds):
-    #     fig.canvas.restore_region(background)
-    #     for l, b in zip(line, buff):
-    #         l.set_ydata(b)
-    #         ax.draw_artist(l)
-    #     fig.canvas.blit(ax.bbox)
 
     # step simulation
     p.stepSimulation()
